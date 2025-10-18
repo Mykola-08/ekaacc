f2bbf2e5972b969397288d62c47f1804d472f147
Mykola Voronin <mvoronink@gmail.com>
2025-10-18 20:33:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1965873331-9f090",
+  "appId": "1:296874779041:web:40f9a26c7ee84f410f6be0",
+  "apiKey": "AIzaSyArmyl3UcrEpZSNR_jl2g7vpXRhvjCR42A",
+  "authDomain": "studio-1965873331-9f090.firebaseapp.com",
+  "measurementId": "G-67QJRENXGL",
+  "messagingSenderId": "296874779041"
+};
